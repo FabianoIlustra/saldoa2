a75f7b1a75de76e791fc8ecd50d877cf65e40d2f
FabianoIlustra <fabianofreitasfoto@hotmail.com>
2026-03-01 20:25:31 -0300

    Configure base path in Vite config
    Set base path for asset resolution on Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Esta linha é crucial para o Cloudflare encontrar os assets corretamente
   plugins: [react()],
   server: {
     host: '0.0.0.0',
