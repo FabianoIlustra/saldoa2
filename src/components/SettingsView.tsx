7c12a8688395cd44c6821e73feee8137385cbc02
FabianoIlustra <fabianofreitasfoto@hotmail.com>
2026-03-02 12:16:50 -0300

    Delete src directory

--- src/components/SettingsView.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SettingsView: React.FC<any> = () => {
-  return <div className="p-4 bg-white rounded-xl shadow-sm">Settings Placeholder</div>;
-};
-
-export default SettingsView;
--- src/components/StatementImporter.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const StatementImporter: React.FC<any> = () => {
-  return <div className="p-4 bg-white rounded-xl shadow-sm">Statement Importer Placeholder</div>;
-};
-
-export default StatementImporter;
