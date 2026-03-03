f432a96b36d2563e57c93ee8c087de9158ae6842
FabianoIlustra <fabianofreitasfoto@hotmail.com>
2026-03-02 21:55:46 -0300

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,14 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-
-// Registro do Service Worker para PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js').then(reg => {
-      console.log('Service Worker registrado com sucesso:', reg.scope);
-    }).catch(err => {
-      console.log('Falha ao registrar Service Worker:', err);
-    });
-  });
-}
+import './index.css';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
